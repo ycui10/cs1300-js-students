2d9b675c9a1d896bee9250ea7459b5a1edcc3bca
ycui10 <60239373+ycui10@users.noreply.github.com>
2020-10-28 07:58:31 +0800

    first commit

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var corsApiUrl = "https://cors-anywhere.herokuapp.com/";
 // TODO: REPLACE YOUR TOKEN
-var apiToken = "?token=YOUR_TOKEN_HERE";
+var apiToken = "?token=GrGzLC8izaSMySU7irpI3yttutdvXZgqOYEVfRDZ_H8";
 
 // CORS stands for "cross origin resource sharing" -- you'll be making http requests in order
 // DON'T CHANGE THIS: fetches the data from the API endpoint
@@ -20,12 +20,32 @@ const corsPromise = () =>
     resolve(request);
   });
 
+const renderData = (response) => {
+
+    const plantData = JSON.parse(response).data;
+
+}
+
+
+
+  corsPromise().then( (request) => request.onload = request.onerror = function() {
+
+      const plantData = JSON.parse(request.response).data;
+      const genus = Array.from(plantData.map(plant => plant.genus));
+      
+
+      document.getElementById('plants').innerHTML = genus;
+  })
+
+  
+
+  
 // THIS IS SOME SAMPLE CODE FOR HOW TO USE PROMISES -- feel free to adapt this into a function!
-corsPromise().then(
-  (request) =>
-    (request.onload = request.onerror = function () {
-      // TODO: ADD FUNCTION, ETC. FOR WHATEVER YOU WANT TO DO ONCE THE DATA IS RECEIVED
-    })
-);
+// corsPromise().then(
+//   (request) =>
+//     (request.onload = request.onerror = function () {
+//       // TODO: ADD FUNCTION, ETC. FOR WHATEVER YOU WANT TO DO ONCE THE DATA IS RECEIVED
+//     })
+// );
 
 //// TODO: ADD WHATEVER FUN CONTENT YOU WANT ////
